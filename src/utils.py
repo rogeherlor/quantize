ee496a200fdde847559527b02c095dbc4108c9b6
rogeherlor <rogeherlor@gmail.com>
2025-07-11 16:10:37 +0000

    Fix imagenet dataloaders

--- src/utils.py
+++ src/utils.py
@@ -17,12 +17,13 @@ import src.scheduler.lr_scheduler as lr_scheduler
 #========================================================================
 def imagenet_dataloaders(batch_size, num_workers, pin_memory, DDP_mode = True, model = None):
     
-    logger.warning("Not definitive imagenet train/test data")
-    traindir = './data/imagenet/training'
-    valdir = './data/imagenet/validation' 
+    logger.warning("Not definitive imagenet train/val data: test/val")
+    traindir = './data/imagenet/train'
+    valdir = './data/imagenet/val'
     
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
+    # The label of the first folder will be 0, the second will be 1, and so on.
     train_dataset = ImageFolder(traindir, transforms.Compose([transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize,]))
     val_dataset = ImageFolder(valdir, transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), normalize,]))
 
